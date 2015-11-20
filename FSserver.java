0613ab9cd96ccac073035ae809a880bac1aca423
wooba93 <wooba93@hanmail.net>
2015-11-20 12:55:06 +0900

    ver.1

--- FSserver.java
+++ FSserver.java
@@ -1,9 +1,19 @@
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.color.ColorSpace;
+import java.awt.image.*;
+
+import javax.imageio.*;
+import javax.imageio.stream.*;
+
+import com.sun.org.apache.xerces.internal.impl.dv.util.*;
+// DATE, USERNAME, BIGFOOD, SMALLFOOD, REGION, PRICE, EVALUE
 
 public class FSserver {
-
+	LinkedList<BoardData> DataLinks = new LinkedList<BoardData>();
 	static int port = 8080;
 	ServerSocket serverSocket = null;
 	Socket socket = null;
@@ -21,38 +31,50 @@ public class FSserver {
 		catch(Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 	class ThreadFunction extends Thread {
 		Socket socket;
 		DataInputStream in;
 		DataOutputStream out;
+		//String name;
+		String name;
 		
 		public ThreadFunction(Socket socket)
 		{
 			this.socket = socket;
-			try{
-				in = new DataInputStream(socket.getInputStream());
-				out = new DataOutputStream(socket.getOutputStream());
-			}
-			catch (Exception e){
-				System.out.println("ThreadFunction initializing failed.: " + e);
-			}
+
 		}
 		@Override
 		public void run()
 		{
-			String name = "";
+			//String name = "";
+			
 			while(true)
 			{
 			try{
+				name = "";
+				
+				in = new DataInputStream(this.socket.getInputStream());
+				out = new DataOutputStream(this.socket.getOutputStream());
+				
 				name = in.readUTF();
+				
+				//name = in.read();
 				if(name.equals("out"))
 				{
 					System.out.println("connection End");
 					break;
 				}
-				out.writeUTF(name + "OK");
+				
+				out.writeUTF("OK");
 				System.out.println(socket + ": " + name);
+				if(name.subSequence(0, 5).equals("WRITE"))
+					saveWriteData(name);
+				if(name.subSequence(0, 5).equals("IMAGE"))
+					savingImage(name);
+				
+				
 			}
 			catch(Exception e){
 				System.out.println("Send & Receive Error: " + e);
@@ -60,11 +82,128 @@ public class FSserver {
 			}
 			
 		}
+		public void savingImage(/*String*/String data) throws IOException
+		{
+			int imgH = in.readInt();
+			System.out.println("imgH: " + imgH);
+			out.writeUTF("ImageH OK");
+			int imgW = in.readInt();
+			System.out.println("imgW: " + imgW);
+			out.writeUTF("ImageW OK");
+			
+			int length = in.readInt();
+			System.out.println("length: " + length);
+			out.writeUTF("Byte Length OK");
+			
+			byte[] base64String = new byte[length];
+			in.read(base64String, 0, length);
+			out.writeUTF("IMAGE READ OK");
+			System.out.println("image read ok");
+			
+			BufferedImage img = ImageIO.read(new ByteArrayInputStream(base64String));
+			ImageIO.write(img, "jpg", new File("C:/Users/ChoiYeojin/Desktop/DesertTest.jpg"));
+			
+		}
+		public void saveImage(String data)
+		{
+			
+			try{
+			
+			//String imgb = "";
+			int imgH = in.readInt();
+			out.writeUTF("ImageH OK");
+			out.flush();
+			int imgW = in.readInt();
+			out.writeUTF("ImageW OK");
+			out.flush();
+			int byteLength = in.readInt();
+			out.writeUTF("bLength OK");
+			out.flush();
+			
+			byte[] imageByte = new byte[byteLength];
+			
+			in.read(imageByte, 0, imgH * imgW);
+			
+			System.out.println("[Image]: " + imageByte.length);
+			out.writeUTF("IMAGE READ OK");
+			out.flush();
+			//imageByte = imgb.getBytes();
+			//BufferedImage image = createRGBImage(imageByte);
+//			FileOutputStream stream = new FileOutputStream("C:/Users/ChoiYeojin/Desktop/DesertTest.jpg");
+			//ImageIO.write(image, "jpg", new File("C:/Users/ChoiYeojin/Desktop/DesertTest.jpg"));
+			
+			ByteArrayInputStream bais = new ByteArrayInputStream(imageByte);
+			Iterator<?> readers = ImageIO.getImageReadersByFormatName("jpg");
+			ImageReader reader = (ImageReader) readers.next();
+			Object source = bais;
+			ImageInputStream iis = ImageIO.createImageInputStream(source);
+			reader.setInput(iis, true);
+			ImageReadParam param = reader.getDefaultReadParam();
+			Image image = reader.read(0, param);
+			BufferedImage bufferedImage = new BufferedImage(imgW, imgH, BufferedImage.TYPE_INT_RGB);
+			Graphics2D g2d = bufferedImage.createGraphics();
+			g2d.drawImage(image, null, null);
+			File imageFile = new File("C:/Users/ChoiYeojin/Desktop/DesertTest.jpg");
+			ImageIO.write(bufferedImage, "jpg", imageFile);
+			System.out.println("IMAGE SAVE");
+			} catch(Exception e) {
+				System.out.println("reading img Failed.");
+			}
+			
+			
+		}
+		public void saveWriteData(String data)
+		{
+			Date now = new Date();
+			BoardData temp = new BoardData();
+			
+			temp.setMiddle5Datas(data.split("/"));
+			temp.setData(0, now.toString());
+			/*try {
+				temp.setData(6, in.readUTF());
+				out.writeUTF("OK");
+				System.out.println(socket + ": evalue");
+			}
+			catch(Exception e)
+			{
+				System.out.println("saveWriteData Error: " + e);
+			}*/
+			DataLinks.add(temp);
+			System.out.println("Success Save new Board Datas: " + DataLinks.size());
+		}
+		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FSserver ms = new FSserver();
 		ms.init();
+
+	}
+	private static BufferedImage createRGBImage(byte[] bytes)
+	{
+		DataBufferByte buffer = new DataBufferByte(bytes, bytes.length);
+		ColorModel cm = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_sRGB), new int[]{8,8,8}, false, false, Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
+		return new BufferedImage(cm, Raster.createInterleavedRaster(buffer, 1024, 768, 1024 * 3, 3, new int[]{0, 1, 2}, null), false, null);
 	}
 
+}
+
+class BoardData {
+	String[] data = new String[7];
+	
+	public String getData(int index)
+	{
+		return data[index];
+	}
+	public void setData(int index, String str)
+	{
+		data[index] = str;
+	}
+	public void setMiddle5Datas(String[] str)
+	{
+		for(int i = 2/*1*/; i < 5/*6*/; i ++)
+		{
+			data[i] = str[i + 1];
+		}
+	}
 }
--- clientTest.java
+++ clientTest.java
@@ -1,43 +1,109 @@
 import java.net.Socket;
+import java.security.spec.EncodedKeySpec;
 import java.util.Scanner;
 import java.io.*;
+import java.awt.image.*;
+import com.sun.org.apache.xerces.internal.impl.dv.util.*;
+
+import javax.imageio.ImageIO;
 
 public class clientTest {
 
 	static int port = 8080;
-	static String ip = "127.0.0.1";
+	static String ip = "10.30.115.215";
 	static Socket socket = null;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		DataInputStream in;
 		DataOutputStream out;
 		Scanner s = new Scanner(System.in);
+		//File imgPath = new File("C:/Users/ChoiYeojin/Desktop/Desert.jpg");
+		//BufferedImage bimg = ImageIO.read(imgPath);
+		/*WritableRaster raster = bimg.getRaster();
+		DataBufferByte data = (DataBufferByte)raster.getDataBuffer();
+		byte[][] dataByte = data.getBankData();*/
+		//FileInputStream fis = new FileInputStream(imgPath);
+		//ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		byte[] buf = new byte[1024];
 		
-
+		//System.out.println("Height: " + bimg.getHeight() + " / Width: " + bimg.getWidth());
 			try {
 				//해당 아이피와 포트로 소켓 연결
 			socket = new Socket(ip, port);
 			System.out.println("Connecting success.");
 			
 			//해당 소켓에서 들어오는 버퍼
-			in = new DataInputStream(socket.getInputStream());
+
 			//해당 소켓에 출력하는 버퍼
-			out = new DataOutputStream(socket.getOutputStream());
+
 			
 			while(true){			
 			String message = "";
 			//사용자가 입력한 문자열을 받음
 			message = s.nextLine();
 			//받은 문자열을 서버에 전송
+			//out.writeUTF(message);
+			in = new DataInputStream(socket.getInputStream());
+			out = new DataOutputStream(socket.getOutputStream());
+			
 			out.writeUTF(message);
+			System.out.println(in.readUTF());
+			
+			ByteArrayOutputStream baos = new ByteArrayOutputStream(1000);
+			BufferedImage img = ImageIO.read(new File("C:/Users/ChoiYeojin/Desktop/Desert.jpg"));
+			
+			out.writeInt(img.getHeight());
+			System.out.println(in.readUTF());
+			
+			out.writeInt(img.getWidth());
+			System.out.println(in.readUTF());
+			
+			ImageIO.write(img,  "jpg", baos);
+			baos.flush();
+			
+			byte[] base64String = baos.toByteArray();//Base64.encode(baos.toByteArray());
+			baos.close();
+			
+			out.writeInt(base64String.length);
+			System.out.println(in.readUTF());
+			
+			out.write(base64String, 0, base64String.length);
+			System.out.println(in.readUTF());
+			
+			/*out.writeInt(dataByte[0].length);
+			System.out.println(in.readUTF());
+
+			for(int i = 0; i < dataByte.length; i ++)
+			{
+				out.write(dataByte[i], 0, dataByte[i].length);
+				out.flush();
+				System.out.println(in.readUTF());
+			}*/
+			/*int knum = 0;
+			for(int readNum; (readNum = fis.read(buf)) != -1;)
+			{
+				baos.write(buf,  0, readNum);
+				System.out.println("read " + readNum + "bytes,");
+				knum ++;
+			}
+			System.out.println("line: " + knum);
+			byte[] dataByte = baos.toByteArray();
+			out.writeInt(dataByte.length);
+			System.out.println(in.readUTF());
+			out.write(dataByte, 0, dataByte.length);
+			out.flush();
+			System.out.println(in.readUTF());*/
+			//System.out.println(in.readUTF());
+		
 			//받은 문자열이 out이면 클라이언트(연결) 종료
 			if(message.equals("out"))
 				break;
 			//서버로부터 메세지 받음
-			message = in.readUTF();
+			//message = in.read();
+			//in.readUTF();
 			//받은 메세지 출력
-			System.out.println(message);
+			System.out.println("message: " + message);
 		}}
 		catch(Exception e)
 		{
