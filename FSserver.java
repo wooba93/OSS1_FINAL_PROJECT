77512ffd050f2d7208ec8d0b2bd3e8e135d75c45
wooba93 <wooba93@hanmail.net>
2015-12-07 19:56:35 +0900

    search data - not complete

--- FSserver.java
+++ FSserver.java
@@ -6,10 +6,9 @@ import java.awt.image.*;
 import javax.imageio.*;
 
 // DATE, USERNAME, FOODNAME, FOODSTYLE, REGION, PRICE, LOCATION, EVALUE
-// SEARCH//
 
 public class FSserver {
-	static LinkedList<BoardData> DataLinks = new LinkedList<BoardData>();
+	static userLinkedList DataLinks = new userLinkedList();
 	static int port = 8080;
 	ServerSocket serverSocket = null;
 	Socket socket = null;
@@ -85,16 +84,8 @@ public class FSserver {
 			}
 			
 		}
-		public void savingImage(/*String*/String data) throws IOException
+		public void savingImage(String data) throws IOException
 		{
-			/*int imgH = in.readInt();
-			System.out.println("imgH: " + imgH);
-			out.write("ImageH OK".getBytes());
-			out.flush();
-			int imgW = in.readInt();
-			System.out.println("imgW: " + imgW);
-			out.write("ImageW OK".getBytes());
-			out.flush();*/
 			int length = in.readInt();
 			System.out.println("length: " + length);
 			out.write("Byte Length OK".getBytes());
@@ -102,9 +93,10 @@ public class FSserver {
 			byte[] baseTemp = new byte[4500];
 			byte[] base64String = new byte[length];
 			int count = 0;
-			for(int i = 0; i < length; i += 4500)
+			int dataLength = 0;
+			for(int i = 0; i < length; i += dataLength)
 			{
-				int dataLength = in.read(baseTemp);
+				dataLength = in.read(baseTemp);
 				System.out.println("ImageByte: " + dataLength);
 				System.arraycopy(baseTemp, 0, base64String, i, dataLength);
 				out.write("IMAGE READ OK".getBytes());
@@ -115,10 +107,11 @@ public class FSserver {
 			System.out.println("image read ok: " + count);
 			
 			BufferedImage img = ImageIO.read(new ByteArrayInputStream(base64String));
-			ImageIO.write(img, "jpg", new File("C:/Users/ChoiYeojin/Desktop/OSS/DesertTest.jpg"));
+			int integer = DataLinks.size() - 1;
+			String s = System.getProperty("user.dir").replace("\\", "/") + "/Images/FOODSHARE" + Integer.toString(integer) + ".jpg";
+			ImageIO.write(img, "jpg", new File(s));
 			
-			DataLinks.getLast().allocImageByte(length);
-			DataLinks.getLast().savingImage(base64String);
+			DataLinks.getLast().savingImage(DataLinks.size() - 1);
 		}
 		public void saveWriteData(byte[] buf) throws IOException
 		{
@@ -141,26 +134,59 @@ public class FSserver {
 		{
 			byte[] buf = new byte[1024];
 			Arrays.fill(buf, (byte) 0);
-			int length = in.read(buf);
+			String[] splitData = data.split("/"); // SEARCH/FOODSTYLE/전체/REGION/전체
 			Iterator<BoardData> iterator = DataLinks.iterator();
-			BoardData temp = DataLinks.getFirst();
-			String dataResult;
-			String bufStr = new String(buf, 0, length);
+			//DataLinks.getFirst();
+			String dataResult = "";
+			
+			out.write(Integer.toString(DataLinks.sizeOfDatas(splitData[2], splitData[4])).getBytes());
+			in.read(buf);
+			Arrays.fill(buf, (byte) 0);
 			while(iterator.hasNext())
 			{
-				String tempStr = temp.getData(2);
-				if(bufStr.equals(tempStr))
+				BoardData temp = iterator.next();
+				String tempStr1 = temp.getData(3);
+				String tempStr2 = temp.getData(4);
+				if(splitData[2].equals("전체") && splitData[4].equals("전체"))
 				{
 					dataResult = temp.getAll();
 					out.write(dataResult.getBytes(), 0, dataResult.getBytes().length);
-					return;
+					in.read(buf);
+					Arrays.fill(buf, (byte) 0);
+				}
+				else if(splitData[2].equals("전체"))
+				{
+					if(splitData[4].equals(tempStr2))
+					{
+						dataResult = temp.getAll();
+						out.write(dataResult.getBytes(), 0, dataResult.getBytes().length);
+						in.read(buf);
+						Arrays.fill(buf, (byte) 0);
+					}
+				}
+				else if(splitData[4].equals("전체"))
+				{
+					if(splitData[2].equals(tempStr1))
+					{
+						dataResult = temp.getAll();
+						out.write(dataResult.getBytes(), 0, dataResult.getBytes().length);
+						in.read(buf);
+						Arrays.fill(buf, (byte) 0);
+					}
 				}
 				else
 				{
-					temp = iterator.next();
+					if(splitData[2].equals(tempStr1) && splitData[4].equals(tempStr2))
+					{
+						dataResult = temp.getAll();
+						out.write(dataResult.getBytes(), 0, dataResult.getBytes().length);
+						in.read(buf);
+						Arrays.fill(buf, (byte) 0);
+					}
 				}
 			}
-			out.write("NODATA".getBytes());
+			//out.write("NODATA".getBytes());
+			System.out.println("SEARCH COMPLETE");
 		}
 		
 	}
@@ -168,21 +194,54 @@ public class FSserver {
 		// TODO Auto-generated method stub
 		FSserver ms = new FSserver();
 		ms.init();
-
 	}
 
 }
-
+class userLinkedList extends LinkedList<BoardData>
+{
+	public int sizeOfDatas(String data1, String data2)
+	{
+		int count = 0;
+		
+		Iterator<BoardData> iterator = this.iterator();
+		
+		while(iterator.hasNext())
+		{
+			BoardData temp = iterator.next();
+			String tempStr1 = temp.getData(3);
+			String tempStr2 = temp.getData(4);
+			if(tempStr1.equals("전체") && tempStr2.equals("전체"))
+			{
+				return this.size();
+			}
+			else if(tempStr1.equals("전체"))
+			{
+				if(tempStr2.equals(data2))
+					count ++;
+			}
+			else if(tempStr2.equals("전체"))
+			{
+				if(tempStr1.equals(data1))
+					count ++;
+			}
+			else
+			{
+				if(tempStr1.equals(data1) && tempStr2.equals(data2))
+					count ++;
+			}
+		}
+		
+		return count;
+	}
+}
 class BoardData {
 	String[] data = new String[8];
-	byte[] image;
-	public void allocImageByte(int length)
+	String imageName;
+	
+	public void savingImage(int i)
 	{
-		image = new byte[length];
-	}
-	public void savingImage(byte[] origin)
-	{
-		image = Arrays.copyOf(origin, origin.length);
+		imageName = System.getProperty("user.dir").replace("\\", "/") + "/Images/FOODSHARE";
+		imageName += (i + ".jpg");
 	}
 	public String getData(int index)
 	{
@@ -190,7 +249,9 @@ class BoardData {
 	}
 	public String getAll()
 	{
-		return (data[0] + "/" + data[1] + "/" + data[2] + "/" + data[3] + "/" + data[4] + "/" + data[5]);
+		String dataStr = data[0] + "/" + data[1] + "/" + data[2] + "/" + data[3] + "/" + data[4] + "/" + data[5] + "/" + data[6] + "/" + data[7] + "//";
+		dataStr += "/////";
+		return dataStr;
 	}
 	public void setData(int index, String str)
 	{
